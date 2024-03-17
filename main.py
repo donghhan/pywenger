adc3c2a98bbc6cb70f8774a88a0dd6ec4893c451
donghhan <apensia914@gmail.com>
2024-03-17 19:37:16 +0900

    basic ui added

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import sys
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine
+from PyQt6 import QtCore, uic
+from PyQt6.QtWidgets import *
+from lib.Pywenger import Ui_MainWindow
+
+
+class Main(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = Main()
+    main.show()
+    app.exec()
